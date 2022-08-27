9cfcd8c69534a83becfd6f9f7bbb08e50aecd95b
Ryunosuke39 <111721351+Ryunosuke39@users.noreply.github.com>
2022-08-28 02:33:57 +1000

    credit is finished

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,19 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
+    //red and blue object placement 
+    Vector3 objectPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectPos = new Vector3(2, 0, 0);
+        Instantiate(redObj, objectPos, Quaternion.identity);
+        objectPos = new Vector3(-2, 0, 0);
+        Instantiate(blueObj, objectPos, Quaternion.identity);
+
     }
 
     // Update is called once per frame
